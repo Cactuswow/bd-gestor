da2bf6a78b7b2b743cf1216c1eaf01027d9dac6b
Kactuswow <bryanalvarezg.kcts@gmail.com>
2024-12-09 07:00:39 -0500

    a

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -77,4 +77,147 @@ router.delete('/customers/:id', async (req, res) => {
   res.json(customers)
 })
 
+router.get('/employees', async (_req, res) => {
+  const pg = new Pg().getClient
+  const employees = await pg.query(
+    `SELECT epe_id as id,
+     concat(first_name, ' ', middle_name) as name,
+     concat(first_last_name, ' ', middle_last_name) as "lastName",
+     phone_number as phone,
+     email,
+     salary,
+     employee_type as "employeeType"
+     FROM sk.employees;
+    `
+  )
+
+  res.json(employees)
+})
+
+router.post('/employees', async (req, res) => {
+  const pg = new Pg().getClient
+  const { name, lastName, phone, email, salary, employeeType } = req.body
+
+  const [firstName, middleName] = name
+  const [firstLastName, middleLastName] = lastName
+
+  const employees = await pg.query(
+    `INSERT INTO sk.employees (first_name, middle_name, first_last_name, middle_last_name, phone_number, email, salary, employee_type)
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+     RETURNING epe_id as id;
+    `,
+    [
+      firstName,
+      middleName,
+      firstLastName,
+      middleLastName,
+      phone,
+      email,
+      salary,
+      employeeType
+    ]
+  )
+
+  res.json(employees)
+})
+
+router.put('/employees/:id', async (req, res) => {
+  const pg = new Pg().getClient
+  const { name, lastName, phone, email, salary, employeeType } = req.body
+
+  const [firstName, middleName] = name
+  const [firstLastName, middleLastName] = lastName
+
+  const employees = await pg.query(
+    `UPDATE sk.employees
+     SET first_name = $1, middle_name = $2, first_last_name = $3, middle_last_name = $4, phone_number = $5, email = $6, salary = $7, employee_type = $8
+     WHERE epe_id = $9;
+    `,
+    [
+      firstName,
+      middleName,
+      firstLastName,
+      middleLastName,
+      phone,
+      email,
+      salary,
+      employeeType,
+      req.params.id
+    ]
+  )
+
+  res.json(employees)
+})
+
+router.delete('/employees/:id', async (req, res) => {
+  const pg = new Pg().getClient
+  const employees = await pg.query(
+    `DELETE FROM sk.employees
+     WHERE epe_id = $1;
+    `,
+    [req.params.id]
+  )
+
+  res.json(employees)
+})
+
+router.get('/orders', async (_req, res) => {
+  const pg = new Pg().getClient
+  const orders = await pg.query(
+    `SELECT odr_id as id,
+     ctr_id as "customerId",
+     epe_id as "employeeId",
+     TO_CHAR(odr_date, 'YYYY-MM-DD') as date,
+     odr_status as status,
+     total_price as price,
+     TO_CHAR(delivery_date, 'YYYY-MM-DD') as "deliveryDate"
+     FROM sk.orders;
+    `
+  )
+
+  res.json(orders)
+})
+
+router.post('/orders', async (req, res) => {
+  const pg = new Pg().getClient
+  const { customerId, employeeId, deliveryDate } = req.body
+
+  const orders = await pg.query(
+    `INSERT INTO sk.orders (ctr_id, epe_id, odr_date, odr_status, total_price, delivery_date)
+     VALUES ($1, $2, $3, $4, $5, $6)
+     RETURNING odr_id as id;
+    `,
+    [customerId, employeeId, new Date(), false, 0, deliveryDate]
+  )
+
+  res.json(orders)
+})
+
+router.put('/orders/:id', async (req, res) => {
+  const pg = new Pg().getClient
+  const { customerId, employeeId, deliveryDate } = req.body
+
+  const orders = await pg.query(
+    `UPDATE sk.orders
+     SET ctr_id = $1, epe_id = $2, delivery_date = $3
+     WHERE odr_id = $4;
+    `,
+    [customerId, employeeId, deliveryDate, req.params.id]
+  )
+
+  res.json(orders)
+})
+
+router.delete('/orders/:id', async (req, res) => {
+  const pg = new Pg().getClient
+  const orders = await pg.query(
+    `DELETE FROM sk.orders
+     WHERE odr_id = $1;
+    `,
+    [req.params.id]
+  )
+
+  res.json(orders)
+})
+
 module.exports = { router }
